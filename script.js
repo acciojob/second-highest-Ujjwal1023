c3a09af0b9cd4b1d912163c0d8079662aec3b2d5
Ujjwal1023 <129600613+Ujjwal1023@users.noreply.github.com>
2023-09-09 20:47:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// let arr = [3, 10, 9, 12, 18, 5];
+// //                i=2
+
+let a = arr[0],
+  b = arr[1];
+
+for (let i = 2; i < arr.length; i++) {
+  let currentMax = a > b ? a : b; //3
+  let currentMin = a < b ? a : b; //
+
+  if (currentMax > arr[i]) {
+    a = currentMin; // a = 3
+    b = arr[i]; // b = 9
+  }
+}
+
+console.log(a > b ? a : b);
